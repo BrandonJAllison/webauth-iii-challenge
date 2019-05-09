d70c8aff7e7dab0b9e9c7ca13815f1a6a6e938d6
Brandon Allison <brandona8128@outlook.com>
2019-05-09 16:17:54 -0400

    front-end working for MVP, might refactor to add auth HOC

--- application/src/App.js
+++ application/src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { NavLink, Route } from 'react-router-dom';
 import './App.css';
+import SignIn from './SignIn/index';
+import SignUp from './SignUp/index';
+import Users from './Users';
 
-function App() {
+const button = {
+  borderRadius: "15px",
+  border: 'none'
+}
+
+const Home = props => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Welcome Home</h3>
     </div>
-  );
+  )
+}
+
+class App extends Component {
+  logout = () => {
+    localStorage.removeItem('jwt');
+    window.location.reload();
+
+  }
+
+  SubmitHandler = (event) => {
+    event.preventDefault();
+    this.setState();
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+            <NavLink to='/' exact> Home</NavLink>&nbsp;&nbsp;&nbsp;
+            <NavLink to='/SignUp'>Sign Up</NavLink>&nbsp;&nbsp;&nbsp;
+            <NavLink to='/SignIn'>Sign In</NavLink>&nbsp;&nbsp;&nbsp;
+            <NavLink to='/Users'>Users Page</NavLink>&nbsp;&nbsp;
+            <button style={button} onClick={this.logout} onSubmit={this.SubmitHandler}>Sign Out</button>
+          </header>
+        
+        <div>
+          <Route path='/' component={Home} exact></Route>
+          <Route path='/SignUp' component={SignUp} exact></Route>
+          <Route path='/SignIn' component={SignIn} exact></Route>
+          <Route path='/Users' component={Users} exact></Route>
+        </div>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
